13183dfd973290246e6dbb5d557f0a2582f80a54
Prajeshpuri <prajeshpuri@gmail.com>
2020-07-05 17:46:53 +0530

    First Commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBESxWnl74d8wKX6MMexJY5bRLf4cb5qL0",
+    authDomain: "angular-session-63eab.firebaseapp.com",
+    databaseURL: "https://angular-session-63eab.firebaseio.com",
+    projectId: "angular-session-63eab",
+    storageBucket: "angular-session-63eab.appspot.com",
+    messagingSenderId: "53965664427",
+    appId: "1:53965664427:web:0547c19617f83fb52787ee"
+  }
 };
